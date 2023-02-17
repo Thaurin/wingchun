9d79503293b340b8e75ac4b538c1ca3c6ef9335f
Ivo van Heel <heel@glr.nl>
2023-02-17 15:22:53 +0100

    Experiment with adding Markdown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://thaurin.github.io',
-    base: '/wingchun',
+  site: 'https://thaurin.github.io',
+  base: '/wingchun',
+  integrations: [mdx()]
 });
